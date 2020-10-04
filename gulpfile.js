bd3e11e2ab2f4b5150a779a428f3dbacf87ac9c0
Andy Catch <andrew.catch@gmail.com>
2020-10-03 22:02:56 -0400

    updated gulp file to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+// gulpfile.js
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task("sass", function () {
+  return (
+    gulp
+      .src("css/app.scss")
+
+      // Saves compiled CSS to chosen folder
+      .pipe(gulp.dest("master.css"))
+  );
+});
+
+gulp.task("watch", function () {
+  gulp.watch("css/app.scss", gulp.series("sass"));
 });
