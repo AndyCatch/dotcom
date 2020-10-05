39d10a356bab6ee729fa0deaae6891eaf6852ddf
Andy Catch <andrew.catch@gmail.com>
2020-10-04 21:13:34 -0400

    refactored gulpfile to remove watch errors

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var sourcemaps = require("gulp-sourcemaps")
 var browserSync = require("browser-sync").create()
 
 // Set a function to compile your .scss files
-function compileCSS() {
+gulp.task("compileCSS", function () {
   return gulp
     .src("src/css/app.scss")
     .pipe(sourcemaps.init())
@@ -20,36 +20,37 @@ function compileCSS() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("dist/css")) // this results in a "app.css" in the dist folder
     .pipe(browserSync.stream())
-}
+})
 
-function html(html) {
+gulp.task("html", function () {
   return gulp.src("src/index.html").pipe(gulp.dest("dist"))
-}
+})
 
-function fonts(fonts) {
+gulp.task("fonts", function () {
   return gulp.src("src/fonts/*").pipe(gulp.dest("dist/fonts"))
-}
+})
 
-function js(js) {
+gulp.task("js", function () {
   return gulp.src("src/js/*.js").pipe(gulp.dest("dist/js"))
-}
+})
 
-function images(images) {
+gulp.task("images", function () {
   return gulp.src("src/images/*").pipe(gulp.dest("dist/images"))
-}
+})
 
 // Sets up a function called watch(), containing the gulp.watch method
-function watch(watch) {
-  browserSync.init({
-    server: {
-      baseDir: "dist",
-    },
-  })
-
+gulp.task("watch", function () {
+  browserSync.init({ server: { baseDir: "dist" } })
   // If any ".html" file is updated then reruns gulp html task to move files to dist folder and also updates live server
-  gulp.watch("src/*.html", gulp.series(html)).on("change", browserSync.reload)
-  gulp.watch("src/css/app.scss", compileCSS)
-}
+  gulp.watch("src/*.html", gulp.series("html")).on("change", browserSync.reload)
+  gulp.watch("src/js/*", gulp.series("js"))
+  gulp.watch("src/fonts/*", gulp.series("fonts"))
+  gulp.watch("src/img/*", gulp.series("images"))
 
-// Runs all the following tasks on "gulp" command
-exports.default = gulp.series(html, compileCSS, js, fonts, images, watch)
+  gulp.watch("src/css/app.scss", gulp.series("compileCSS"))
+})
+
+gulp.task(
+  "default",
+  gulp.parallel("html", "compileCSS", "js", "fonts", "images", "watch")
+)
