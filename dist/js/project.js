1115662f5313a7548cf0bea39553fd50c9f63fa6
Andy Catch <andrew.catch@gmail.com>
2021-04-20 21:18:13 -0400

    package additions

--- dist/js/project.js
+++ dist/js/project.js
@@ -1,82 +1,70 @@
-const title = "TITLE OF PROJECT";
-const mobileCover = document.querySelector(
-  '.cover_title div[data-content-for="xs"] h1 span'
-);
-const breakpoints = Array.from(["xl", "lg", "md", "sm", "xs"]);
-
-let projectTitles = []; // Array
-let marqueeText; // String
-let projectImage; // Element
-let marquee; // Element
-
-public_projectName = title;
-
-isProject();
+isProject()
 
 function isProject() {
-  projectImage = document.getElementsByClassName("project-cover")[0];
-  marquee = document.querySelector(".cover_title h1 span");
+  let title = 'Homepage Vision Verizon' // setting a title
+  let mobileCover = document.querySelector(
+    '.cover_title div[data-content-for="xs"] h1 span'
+  ) // setting a mobile cover (Not sure what this is tbh)
+
+  let marqueeText // String
+  marqueeText = fillArray(title)
+
+  let projectImage // Element
+  projectImage = document.getElementsByClassName('project-cover')[0] //
+
+  let marquee // Element
+  marquee = document.querySelector('.cover_title h1 span')
+
+  // sets to faded out
 
   if (projectImage) {
-    projectImage.classList.add("fade-out");
+    projectImage.classList.add('fade-out')
   }
 
   if (marquee) {
-    marquee.classList.add("fade-out");
+    marquee.classList.add('fade-out')
   }
 
-  setProjectTitles(); // pushes project title elements to an array
-}
+  // pushes project title elements to an array
+  let breakpoints = Array.from(['xl', 'lg', 'md', 'sm', 'xs']) //
+  let projectTitles = [] // Array
 
-function setProjectTitles() {
   for (let i = 0; i < breakpoints.length; i++) {
     let selectorPath = `.cover_title div[data-content-for=${JSON.stringify(
       breakpoints[i]
-    )}] h1 span`;
-    projectTitles.push(document.querySelector(selectorPath));
+    )}] h1 span`
+    projectTitles.push(document.querySelector(selectorPath)) // finds all the elements in the DOM that will have titles to account for responsiveness
   }
 
-  marqueeText = fillArray(public_projectName);
-
-  populate();
-  fadeIn();
+  makeMarquee(projectTitles, marqueeText)
+  fadeIn(projectImage, marquee)
 }
 
-function fadeIn() {
+function fadeIn(projectImage, marquee) {
   if (projectImage) {
-    projectImage.classList.remove("fade-out");
-    projectImage.classList.add("fade-in");
+    projectImage.classList.remove('fade-out')
+    projectImage.classList.add('fade-in')
   }
 
   setTimeout(function () {
     if (marquee) {
-      marquee.classList.remove("fade-out");
-      marquee.classList.add("title-fade-in");
+      marquee.classList.remove('fade-out')
+      marquee.classList.add('title-fade-in')
     }
-  }, 500);
+  }, 500)
 }
 
 function fillArray(titleName) {
-  let string = new Array(50).fill(titleName).join(" — ");
-  titleName = string;
+  let string = new Array(50).fill(titleName).join(' — ')
+  titleName = string
 
-  return titleName;
+  return titleName
 }
 
 function makeMarquee(elArr, text) {
   for (let i = 0; i < elArr.length; i++) {
     if (elArr[i]) {
-      elArr[i].innerHTML = text;
-    }
-
-    if (elArr[i] && elArr[i] === mobileCover) {
-      mobileCover.style.fontSize = 40 + "px";
+      elArr[i].innerHTML = text
     }
   }
-}
-
-function populate() {
-  makeMarquee(projectTitles, marqueeText);
-  // resets length
-  projectTitles = [];
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,62 @@
 // gulpfile.js
-var gulp = require("gulp")
-var sass = require("gulp-dart-sass")
-var cleanCSS = require("gulp-clean-css")
-var sourcemaps = require("gulp-sourcemaps")
+var gulp = require('gulp')
+var sass = require('gulp-dart-sass')
+var cleanCSS = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
 
-var imagemin = require("gulp-imagemin")
+var imagemin = require('gulp-imagemin')
 
-var browserSync = require("browser-sync").create()
+var browserSync = require('browser-sync').create()
 
 // Set a function to compile your .scss files
-gulp.task("compileCSS", function () {
+gulp.task('compileCSS', function () {
   return gulp
-    .src("src/css/app.scss")
+    .src('src/css/app.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(
       cleanCSS({
-        compatibility: "ie8",
+        compatibility: 'ie8',
         rebase: false,
       })
     )
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("dist/css")) // this results in a "app.css" in the dist folder
+    .pipe(gulp.dest('dist/css')) // this results in a "app.css" in the dist folder
     .pipe(browserSync.stream())
 })
 
-gulp.task("html", function () {
-  return gulp.src("src/*.html").pipe(gulp.dest("dist"))
+gulp.task('html', function () {
+  return gulp.src('src/*.html').pipe(gulp.dest('dist'))
 })
 
-gulp.task("fonts", function () {
-  return gulp.src("src/fonts/*").pipe(gulp.dest("dist/fonts"))
+gulp.task('fonts', function () {
+  return gulp.src('src/fonts/*').pipe(gulp.dest('dist/fonts'))
 })
 
-gulp.task("js", function () {
-  return gulp.src("src/js/*.js").pipe(gulp.dest("dist/js"))
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js').pipe(gulp.dest('dist/js'))
 })
 
-gulp.task("images", function () {
+gulp.task('images', function () {
   return gulp
-    .src("src/images/*")
+    .src('src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"))
+    .pipe(gulp.dest('dist/images'))
 })
 
 // Sets up a function called watch(), containing the gulp.watch method
-gulp.task("watch", function () {
-  browserSync.init({ server: { baseDir: "dist" } })
+gulp.task('watch', function () {
+  browserSync.init({ server: { baseDir: 'dist' } })
   // If any ".html" file is updated then reruns gulp html task to move files to dist folder and also updates live server
-  gulp.watch("src/*.html", gulp.series("html")).on("change", browserSync.reload)
-  gulp.watch("src/js/*.js", gulp.series("js"))
-  gulp.watch("src/fonts/*", gulp.series("fonts"))
-  gulp.watch("src/img/*", gulp.series("images"))
+  gulp.watch('src/*.html', gulp.series('html')).on('change', browserSync.reload)
+  gulp.watch('src/js/*.js', gulp.series('js'))
+  gulp.watch('src/fonts/*', gulp.series('fonts'))
+  gulp.watch('src/img/*', gulp.series('images'))
 
-  gulp.watch("src/css/app.scss", gulp.series("compileCSS"))
+  gulp.watch('src/css/app.scss', gulp.series('compileCSS'))
 })
 
 gulp.task(
-  "default",
-  gulp.parallel("html", "compileCSS", "js", "fonts", "images", "watch")
+  'default',
+  gulp.parallel('html', 'compileCSS', 'js', 'fonts', 'images', 'watch')
 )
