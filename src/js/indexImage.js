177379e6193d8f6fe0812483c6e4eee32d5d1341
Andy Catch <andrew.catch@gmail.com>
2021-07-17 16:55:43 -0400

    More updates indexImage. Cleaned structure

--- src/js/indexImage.js
+++ src/js/indexImage.js
@@ -3,36 +3,30 @@ function indexImage(itemsNodeList) {
   let header = document.querySelector('h1')
 
   for (let i = 0; i < indexItems.length; i++) {
-    let wrappers = Array.from(
-      indexItems[i].querySelectorAll('div.image-set-wrapper')
-    )
-
-    let numLabel = document.createElement('p')
-    numLabel.classList.add('numImages')
-    numLabel.innerHTML = 0
-    if (wrappers[0]) {
-      wrappers[0].appendChild(numLabel)
-    }
     let bgHover = document.createElement('div')
     bgHover.classList.add('thumbBg')
 
     indexItems[i].appendChild(bgHover)
 
     indexItems[i].addEventListener('mouseover', function (event) {
+      let current = event.currentTarget
       indexItems.forEach((item) => {
         item.style.zIndex = 0 // reset all to 0
       })
 
       bgHover.style.opacity = 1
-      numLabel.style.color = 'var(--black)'
       header.classList.add('headingHover')
-      event.currentTarget.style.zIndex = 1 // bring the current to 1
+      current.getElementsByClassName('numImages')[0].style.color =
+        'var(--black)'
+      current.style.zIndex = 1 // bring the current to 1
     })
 
     indexItems[i].addEventListener('mouseout', function (event) {
+      let current = event.currentTarget
       bgHover.style.opacity = 0
       header.classList.remove('headingHover')
-      numLabel.style.color = 'var(--white)'
+      current.getElementsByClassName('numImages')[0].style.color =
+        'var(--white)'
     })
 
     if (indexItems[i].querySelectorAll('div.image-set')[i]) {
@@ -40,7 +34,9 @@ function indexImage(itemsNodeList) {
       let imageSets = Array.from(
         indexItems[i].getElementsByClassName('image-set')
       )
-      numLabel.innerHTML = imageSets.length
+
+      let numImagesLabel = indexItems[i].getElementsByClassName('numImages')[0]
+      numImagesLabel.innerHTML = imageSets.length
 
       imageSets.forEach((imageSet) => {
         let smlImage = new Image()
